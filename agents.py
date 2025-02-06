d19d614566548ff660b97c1d64544bf29024a41d
Joel Mathew Paul <joelmathewpaul@Mac.lan>
2025-02-06 15:44:51 -0500

    first working version

--- agents.py
+++ agents.py
@@ -1,44 +1,46 @@
 from crewai import Agent
 from textwrap import dedent
-from langchain_community.llms import OpenAI, Ollama
+from langchain_community.llms import OpenAI
+from langchain_ollama import OllamaLLM
+
 from langchain_openai import ChatOpenAI
-
-
-
+from Tools.search_tool import SearchTools
+from Tools.calculator_tool import CalculatorTools
 """
---Work Backwards from the goal
--- define the captain of the crew who orient the other agents towards the goal
---define the experts the captain need to communicate with and delegat tasks to
+Creating Agents Cheat Sheet:
+- Think like a boss. Work backwards from the goal and think which employee 
+    you need to hire to get the job done.
+- Define the Captain of the crew who orient the other agents towards the goal. 
+- Define which experts the captain needs to communicate with and delegate tasks to.
+    Build a top down structure of the crew.
+
+Goal:
+- Create a 7-day travel itinerary with detailed per-day plans,
+    including budget, packing suggestions, and safety tips.
+
+Captain/Manager/Boss:
+- Expert Travel Agent
+
+Employees/Experts to hire:
+- City Selection Expert 
+- Local Tour Guide
 
 
-
-Goal : 7 day travel itinerary with detailed per-day plans including budget , packing suggestions and safety tips
-
-
-Captain/Manager/Boss : 
- --EXpert travel agent
-
-
- Employees :
- -City selection expert
- -Local tour guide 
-
-Notes : 
--Agents should be result driven and have a clear goal in mind
--Role is their job titles
--Goals should be actionable
--Backstory should be their resume
+Notes:
+- Agents should be results driven and have a clear goal in mind
+- Role is their job title
+- Goals should actionable
+- Backstory should be their resume
 """
-
 
 # This is an example of how to define custom agents.
 # You can define as many agents as you want.
 # You can also define custom tasks in tasks.py
-class CustomAgents:
+class TravelAgents:
     def __init__(self):
         self.OpenAIGPT35 = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
         self.OpenAIGPT4 = ChatOpenAI(model_name="gpt-4", temperature=0.7)
-        self.Ollama = Ollama(model="openhermes")
+        self.OllamaLLM = OllamaLLM(model="openhermes")
 
     def expert_travel_agent(self):
         return Agent(
@@ -47,10 +49,10 @@ class CustomAgents:
             goal=dedent(f"""Createa a 7 day travel iterinary, 
                         include budget, 
                         packing suggestions and safety tips"""),
-            # tools=[tool_1, tool_2],
+            tools=[SearchTools.search_internet, CalculatorTools.calculate],
             allow_delegation=False,
             verbose=True,
-            llm=self.OpenAIGPT35,
+            llm=self.OpenAIGPT4,
         )
 
     def city_selection_expert(self):
@@ -58,10 +60,10 @@ class CustomAgents:
             role="City Selection expert",
             backstory=dedent(f"""EXpert in selecting the best cities based on weather, seasons and travelers interest"""),
             goal=dedent(f"""select the best cities to visit based on weather, season, price and traveler interest"""),
-            # tools=[tool_1, tool_2],
+            tools=[SearchTools.search_internet],
             allow_delegation=False,
             verbose=True,
-            llm=self.OpenAIGPT35,
+            llm=self.OpenAIGPT4,
         )
     
     def local_tour_guide(self):
@@ -69,8 +71,8 @@ class CustomAgents:
             role="Local Tour guide",
             backstory=dedent(f"""Ton of experience with cities"""),
             goal=dedent(f"""provide the best insights about the city selectd"""),
-            # tools=[tool_1, tool_2],
+            tools=[SearchTools.search_internet],
             allow_delegation=False,
             verbose=True,
-            llm=self.OpenAIGPT35,
+            llm=self.OpenAIGPT4,
         )
